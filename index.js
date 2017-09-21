a48201c017d5700ae5ca139f9824cb83d203e00b
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-21 15:52:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function theBeatlesPlay(musicians, instruments) {
   var array = []
   if(length.musicians > 0 && length.musicians >= length.instruments)
   for (i = 0; i < length.musicians; i++) {
-    array.push('${musicians[i]} plays ${instruments[i]}')
+    array.push['${musicians[i]} plays ${instruments[i]}']
   }
   elseif(length.instruments > 0 && length.instruments >= length.musicians)
   for (i=0; i < length.instruments; i++) {
-    array.push('${musicians[i]} plays ${instruments[i]}')
+    array.push['${musicians[i]} plays ${instruments[i]}']
   }
   return array
 }
