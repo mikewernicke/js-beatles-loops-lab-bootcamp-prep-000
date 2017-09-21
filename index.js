1e62823b498fafd00fca696eb50ddb32f9302ef1
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-21 15:50:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  if(length.musicians > 0 && length.musicians >= length.instruments)
+  for (i = 0, i < length.musicians, i++) {
+    array.push('${musicians[i]} plays ${instruments[i]}')
+  }
+  elseif(length.instruments > 0 && length.instruments >= length.musicians)
+  for (i=0, i < length.instruments, i++) {
+    array.push('${musicians[i]} plays ${instruments[i]}')
+  }
+  return array
+}
+function johnLennonFacts(factsArray) {
+  
+}
+function iLoveTheBeatles(n) {
+  var emphaticArray = []
+}
